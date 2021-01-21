737c988efe92d6580f6d717107dfeb3846b11ed8
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-01-21 08:41:24 -0300

    Oauth, security and login plus seeding data base.

---
+++ backend/src/main/java/com/jfb/movieflix/components/JwtTokenEnhancer.java
@@ -0,0 +1,36 @@
+package com.jfb.movieflix.components;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.jfb.movieflix.entities.User;
+import com.jfb.movieflix.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        User user = userRepository.findByEmail(authentication.getName()); // <- Busca o username que vai esta dentro de
+                                                                          // authentication;
+        Map<String, Object> map = new HashMap<>();
+        map.put("userName", user.getName());
+        map.put("userId", user.getId());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(map);
+
+        return accessToken;
+    }
+
+}
--- backend/src/main/java/com/jfb/movieflix/entities/User.java
+++ backend/src/main/java/com/jfb/movieflix/entities/User.java
@@ -1,18 +1,47 @@
 package com.jfb.movieflix.entities;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements UserDetails {
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String name;
+
+  @Column(unique = true)
   private String email;
+
   private String password;
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+    name = "tb_user_role", 
+    joinColumns = @JoinColumn(name = "user_id"), 
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles = new HashSet<>();
 
   public User() {
@@ -79,6 +108,36 @@ public class User implements Serializable {
   @Override
   public int hashCode() {
     return Objects.hashCode(id);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 
 }
